3a67186bc6fbfce8bb6ed105bcd1f0bed3010fac
Harry Kreicers <104709277+crikeyk@users.noreply.github.com>
2023-05-28 13:25:24 +1000

    Update starfield.js

--- starfield.js
+++ starfield.js
@@ -1,34 +1,33 @@
-// Create the starfield effect
-(function() {
-    // Get the canvas element
+(function () {
     var canvas = document.createElement('canvas');
     canvas.id = 'starfield';
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     document.body.appendChild(canvas);
 
-    // Set up the drawing context
     var ctx = canvas.getContext('2d');
 
-    // Array to store the stars
     var stars = [];
 
-    // Create the stars
-    for (var i = 0; i < 200; i++) {
+    function createStar() {
         var star = {
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
             radius: Math.random() * 1.5,
-            speed: Math.random() * 0.5 + 0.1
+            speed: Math.random() + 0.1
         };
         stars.push(star);
     }
 
-    // Function to animate the starfield
+    function createStars() {
+        for (var i = 0; i < 200; i++) {
+            createStar();
+        }
+    }
+
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Draw the stars
         ctx.fillStyle = '#ffffff';
         for (var i = 0; i < stars.length; i++) {
             var star = stars[i];
@@ -36,7 +35,6 @@
             ctx.arc(star.x, star.y, star.radius, 0, 2 * Math.PI);
             ctx.fill();
 
-            // Update the star's position
             star.x -= star.speed;
             if (star.x < 0) {
                 star.x = canvas.width;
@@ -46,12 +44,12 @@
         requestAnimationFrame(animate);
     }
 
-    // Start the animation
+    createStars();
     animate();
 
-    // Resize the canvas when the window is resized
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+        createStars();
     });
 })();
